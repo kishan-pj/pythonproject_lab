e52507cf89ea76361de29925faaf927d193fbf1f
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-03-09 21:17:02 +0545

    1234

---
+++ addition.py
@@ -0,0 +1,7 @@
+# write a program to print sum of the  three numbers
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
+sum_num=a+b+c
+print(f"the sum of number:",sum_num)
