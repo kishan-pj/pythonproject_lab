7535f8495bc64f4bc1252ea88093af6f50ab44bf
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-03-10 06:50:25 +0545

    initial commit

---
+++ area of right angle triangle.py
@@ -0,0 +1,8 @@
+# Write a program that reads the length of the base and height of a right angle triangle and print the area.
+# every number is given in separate line.
+
+a=float(input("enter value of base:"))
+b=float(input("enter value of height:"))
+area=(a*b)//2
+print("The area of right angle triangle is :",area)
+
