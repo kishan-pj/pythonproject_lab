4d89ce4c73e3f041222cde3f97021cc8ef65f821
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-04-11 04:52:10 +0545

    123

---
+++ pythonproject_lab2/positive_negative_zero.py
@@ -0,0 +1,10 @@
+# for given integer x print true if it is positive,print false if it is negative and print
+# zero if it is 0.
+
+x=int(input("enter the number: "))
+if x>0:
+    print("the number is positive ")
+elif x<0:
+    print("the number is negative ")
+else:
+    print("the number is zero ")
