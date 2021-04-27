2c0f711ae9787382f25c5b7fae8b0e8b58d0e7ed
Kishan Prasad Jaiswal <bhootk123@gmail.com>
2021-04-27 12:10:33 +0545

    123

---
+++ pythonproject_lab2/Qno.3odd_even.py
@@ -0,0 +1,7 @@
+# check whether the input given by use is odd or even.
+n=int(input("enter the number: "))
+num= n % 2
+if num==0:
+    print("the number is even")
+else:
+    print("the number is odd")
